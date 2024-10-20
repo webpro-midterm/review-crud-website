fe24b8d0ce1112fafe56f18b93adbcb56aac0357
Enyytime <rogeliokennya@gmail.com>
2024-10-20 19:30:07 +0700

    update backend 2

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MovieController extends Controller
-{
-    //
+class MovieController extends Controller {
+
+  public function index() { //retrieves all the movies in the database 
+    $movies = Movie::all(); //Movies::all() means all movies
+    return view('movies.index', compact('movies')); // compact() is function to creates an array containin all the movies variable in the DB
+    // also this means that it's going to the index.blade.php
+  } 
+
+  public function create() {
+    return view('movies.create'); // creating a movie
+    /*
+    basically movies.create is instruction the code to find the /resources/views/movies directory
+    and contains a file create.blade.php (i haven't made it)
+    */
+  }
+
+  public function store(Request $request) { //after creating a movie you have to make the revies
+    $request->validate([ //to make a review ofcourse you need to fill some form, and alll of that form must be filled
+      'title' => 'required',
+      'description' => 'required',
+      'release_date' => 'required|date',
+    ]);
+
+    Movie::create($request->all());
+
+    return redirect()->route('movie.index'); 
+  }
+
+  public function show() {  //showing a movie
+    return view('movies.show', compact('movie'));
+  }
+
+  public function edit() { // editing a movie
+    return view('movies.edit', compact('movie')); 
+  }
+
+  public function update(Request $request, Movie $movies){
+    $request->validate([
+        'title' => 'required',
+        'description' => 'required',
+        'release_date' => 'required|date',
+    ]);
+
+    $movie->update($request->all());
+
+    return redirect()->route('movies.index');
+  }
+
+  public function destroy(Movie $movie) {
+    $movie->delete();
+    return redirect()->route('movies.index');
+  }
 }
--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,7 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ReviewController extends Controller
-{
-    //
+class ReviewController extends Controller {
+    
+  public function store(Request $request, Movie $movie) {
+
+    $request->validate([
+      'rating' => 'required|integer|min:1|max:5',
+      'review' => 'required',
+    ]);
+    $movie->reviews()->create([
+      'user_id' => auth()->id(),
+      'rating' => $request->rating,
+      'review' => $request->review,
+    ]);
+
+    return redirect()->route('movies.show', $movie);
+
+  }
+
+  public function destroy(Movie $movie, Review $review){
+
+    if ($review->user_id == auth()->id()) {
+      $review->delete();
+    }
+
+    return redirect()->route('movies.show', $movie);
+
+  }
 }
